8fe10d4ba83328814a52092f249db64f8a464a6f
Yawen Guan <yawen.guan.email@gmail.com>
2024-07-05 05:17:03 +0200

    Initial commit.

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(fira-code-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'projectile-grep 'disabled nil)
+(put 'company-coq-fold 'disabled nil)
+(put 'customize-variable 'disabled nil)
