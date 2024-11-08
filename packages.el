0b81119aed0ceea7aa95c4f470c4305139d110f9
Yawen Guan <yawen.guan.email@gmail.com>
2024-11-08 16:50:05 +0100

    Add treemacs-all-the-icons.

--- packages.el
+++ packages.el
@@ -12,6 +12,7 @@
 (package! org-fragtog)
 (package! math-symbol-lists)
 (package! ligature)
+(package! treemacs-all-the-icons)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
